93f515955ea289db116effd1e99d495db1cd2efa
Reeshimevcha <159103614+Reeshimevcha@users.noreply.github.com>
2026-03-15 10:21:17 +0530

    Add databaseURL to Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -6,4 +6,5 @@ const firebaseConfig = {
   messagingSenderId: "355987303332",
   appId: "1:355987303332:web:a516052ee354afa174ef31",
   measurementId: "G-7EFMT3C0K0"
+  databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
