cb9e88347650cf35b639f7e828acc5a6a07ed05f
Reeshimevcha <159103614+Reeshimevcha@users.noreply.github.com>
2026-03-15 10:31:05 +0530

    Add databaseURL to Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBph8A8njpDLIwE9JfB_Yr7j9CdKvKqDQA",
   authDomain: "voidchat-72ed4.firebaseapp.com",
@@ -7,4 +8,5 @@ const firebaseConfig = {
   appId: "1:355987303332:web:a516052ee354afa174ef31",
   measurementId: "G-7EFMT3C0K0"
   databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app"
+
 };
