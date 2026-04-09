de67d52dfa8305dcfc90df9c959ff7285b001ec2
RISHIMEVCHA <159103614+Reeshimevcha@users.noreply.github.com>
2026-04-09 21:33:24 +0530

    Modify Firebase configuration settings
    Updated Firebase storage bucket and app ID, added measurement ID.
    
    Signed-off-by: RISHIMEVCHA <159103614+Reeshimevcha@users.noreply.github.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
+
 // Firebase Configuration — VoidChat
 const firebaseConfig = {
   apiKey: "AIzaSyAMfnx1Ia87oJwRXQfuvCtBnZxWOi8ZbQ4",
   authDomain: "voidchat-72ed4.firebaseapp.com",
   databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "voidchat-72ed4",
-  storageBucket: "voidchat-72ed4.appspot.com",
+  storageBucket: "voidchat-72ed4.firebasestorage.app",
   messagingSenderId: "355987303332",
-  appId: "1:355987303332:web:a516052ee354afa174ef31"
+  appId: "1:355987303332:web:ad21c2d3056e075074ef31",
+  measurementId: "G-17Q02FPNND"
 };
